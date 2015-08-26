58bcdc927a1f52e83c5546045728d61b359798e4
bhivert <hivert.benoit@gmail.com>
2015-08-26 07:59:14 +0200

    up

--- srcs/fdf_create_ui.c
+++ srcs/fdf_create_ui.c
@@ -1,6 +1,15 @@
 
 #include "fdf.h"
 
+void	fdf_key_hook(void *param, int code, int type)
+{
+	(void)param;
+	if (type == 2)
+		return ;
+	if (code == UI_KEY_ESC)
+		exit(EXIT_SUCCESS);
+}
+
 int		fdf_create_ui(t_env *e)
 {
 	if (!(e->win = ui_new_window(e->av[0], 800, 600)))
@@ -11,6 +20,6 @@ int		fdf_create_ui(t_env *e)
 		return (ft_printf("%s unable to create image.\n"), -1);
 	ui_widget_set_color(e->win, e->img_id, 0x000000);
 	ui_widget_set_hook_param(e->win, e->img_id, e->img_id, e);
-	// set key press hook
+	ui_window_set_key_hook(e->win, &fdf_key_hook, NULL);
 	return (0);
 }
