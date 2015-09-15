4b671e092b445003f026383ae8fad348f85c4233
Benoit HIVERT <bhivert@e1r7p9.42.fr>
2015-09-15 16:43:44 +0200

    up

--- srcs/fdf_create_ui.c
+++ srcs/fdf_create_ui.c
@@ -18,13 +18,13 @@ void		fdf_key_hook(void *param, int code, int type)
 	if (code == UI_KEY_ESC)
 		fdf_exit(e);
 	else if (code == UI_KEY_RIGHT || code == UI_KEY_PAD_6)
-		e->rot_Z = (e->rot_Z + 15) % 360;
-	else if (code == UI_KEY_LEFT || code == UI_KEY_PAD_4)
 		e->rot_Z = (e->rot_Z + 360 - 15) % 360;
+	else if (code == UI_KEY_LEFT || code == UI_KEY_PAD_4)
+		e->rot_Z = (e->rot_Z + 15) % 360;
 	else if (code == UI_KEY_UP || code == UI_KEY_PAD_8)
-		e->rot_X = (e->rot_X + 360 - 15) % 360;
-	else if (code == UI_KEY_DOWN || code == UI_KEY_PAD_5)
 		e->rot_X = (e->rot_X + 15) % 360;
+	else if (code == UI_KEY_DOWN || code == UI_KEY_PAD_5)
+		e->rot_X = (e->rot_X + 360 - 15) % 360;
 	else if (code == UI_KEY_PUP || code == UI_KEY_PAD_P)
 		++e->scaling;
 	else if (code == UI_KEY_PDW || code == UI_KEY_PAD_M)
--- srcs/fdf_draw.c
+++ srcs/fdf_draw.c
@@ -79,18 +79,21 @@ static void		fdf_generate_matrix(t_env *e)
 {
 	t_vector	up;
 
-	e->trans_mtx0 = matrix_trans(-((double)e->max_line / 2.0), \
-				-((double)ft_size(e->file) / 2.0), \
-				0);
-	e->trans_mtx1 = matrix_trans((double)e->win->size.width / 2.0, \
-				(double)e->win->size.height / 2.0, \
-				0);
 	e->scale_mtx = matrix_scale(e->scaling, e->scaling, e->scaling);
 	e->rot_X_mtx = matrix_rotx(rad(e->rot_X));
+
 	up = new_vector(0, 0, 1, NULL);
 	up = vector_mult_matrix(&up, e->rot_X_mtx);
 	e->rot_Z_mtx = matrix_axis_rot(up.x, up.y, up.z, rad(e->rot_Z));
+
+	e->trans_mtx0 = matrix_trans(-((double)e->max_line / 2.0), \
+				-((double)ft_size(e->file) / 2.0), \
+				0);
+
 	e->iso_proj = matrix_iso_proj();
+	e->trans_mtx1 = matrix_trans((double)e->win->size.width / 2.0, \
+				(double)e->win->size.height / 2.0, \
+				0);
 }
 
 static void		fdf_free_matrix(t_env *e)
